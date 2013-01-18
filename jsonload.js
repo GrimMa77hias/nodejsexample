49c7a86fd6aa7e521036a0cfe5d07df971e629cb
GrimMa77hias <grim.ma77hias@gmail.com>
2013-01-17 15:04:49 -0900

    Changed it to give pages by reading files instead of doing so explicitly

--- jsonload.js
+++ jsonload.js
@@ -7,19 +7,19 @@ var http = require("http");
 // Create the server. Function passed as parameter is called on every request made.
 // request variable holds all request parameters
 // response variable allows you to do anything with response sent to the client.
+
+//get the webpage from a file
+page = require("fs");
+
 http.createServer(function (request, response) {
 	// Attach listener on end event.
 	// This event is called when client sent all data and is waiting for response.
 	request.on("end", function () {
-		// Write headers to the response.
-		// 200 is HTTP status code (this one means success)
-		// Second parameter holds header fields in object
-		// We are sending plain text, so Content-Type should be text/plain
-		response.writeHead(200, {
-			'Content-Type': 'text/plain'
-		});
+		page.readFile("form.html", 'utf-8', function (error, data){		
+		response.writeHead(200, {'Content-Type': 'text/html'});		
 		// Send data and end response.
-		response.end("<html><title>Form</title><form name=\"submit\" action=\"jsonload.js\" method=\"post\"><input type=\"text\" name=\"headTitle\"><input type=\"text\" name=\"text\"></form></html>");
+		response.end(data);
+		});
 	});
 // Listen on the 1337 port.
 }).listen(1337);
